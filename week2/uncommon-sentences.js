c3e849e5340b11bc78671d1fd11736820f5e52e1
EricB <ericblancas23@gmail.com>
2019-05-10 09:13:17 -0700

    uncommon-sentences

---
+++ week2/uncommon-sentences.js
@@ -0,0 +1,12 @@
+var uncommonFromSentences = function(A, B) {
+    const map = new Map();
+    count(A);
+    count(B);
+    return Array.from(map)
+      .filter(([, count]) => count === 1)
+      .map(([key]) => key);
+  
+    function count(str) {
+      str.split(' ').forEach(word => map.set(word, map.get(word) + 1 || 1));
+    }
+  };
