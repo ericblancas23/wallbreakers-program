5d62326ec9dc7e630928002ae13386c6e5aaca9d
EricB <ericblancas23@gmail.com>
2019-05-12 19:31:58 -0700

    find the difference

---
+++ week2/find-the-difference.js
@@ -0,0 +1,17 @@
+var findTheDifference = function(s, t) {
+    const hashMap = {};
+    for (var i = 0, l = s.length; i < l; i++) {
+        if (!hashMap[s[i]]) {
+            hashMap[s[i]] = 1;
+        } else {
+            hashMap[s[i]]++;
+        }
+    }
+    for (var i = 0, l = t.length; i < l; i++){
+        if (!hashMap[t[i]]) {
+            return t[i];
+        } else {
+            hashMap[t[i]]--;
+        }
+    }
+}; 
